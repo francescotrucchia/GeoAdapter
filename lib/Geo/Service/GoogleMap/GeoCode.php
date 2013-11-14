aa5592727170f186e30aff01c1aaa5f96959dcec
Francesco Trucchia <francesco@trucchia.it>
2013-11-15 00:40:19 +0100

    Some fix

--- lib/Geo/Service/GoogleMap/GeoCode.php
+++ lib/Geo/Service/GoogleMap/GeoCode.php
@@ -3,9 +3,6 @@
 /**
  * This file is part of the GeoAdapter software.
  * (c) 2011 Francesco Trucchia <francesco@trucchia.it>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  */
 
 namespace Geo\Service\GoogleMap;
