b7d500560617c803c1253238014445aebc58811b
Francesco Trucchia <francesco@trucchia.it>
2013-11-15 02:08:08 +0100

    Remove distance method

--- lib/Geo/Location.php
+++ lib/Geo/Location.php
@@ -81,20 +81,4 @@ class Location
         return $this->address;
     }
 
-    /**
-     * Measure the distance between this point and another point
-     * 
-     * @param Location $location
-     * @return float
-     */
-    public function distance(Location $location)
-    {
-        $latA = deg2rad($this->latitude);
-        $lonA = deg2rad($this->longitude);
-        $latB = deg2rad($location->getLatitude());
-        $lonB = deg2rad($location->getLongitude());
-
-        return sprintf('%.2f', acos(sin($latA) * sin($latB) + cos($latA) * cos($latB) * cos($lonB - $lonA)) * 6378.1370);
-    }
-
 }
--- test/lib/Geo/LocationTest.php
+++ test/lib/Geo/LocationTest.php
@@ -37,21 +37,4 @@ class LocationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('via montenapoleone, Roma, Italia', $this->object->getAddress());
     }
 
-    public function testDistance()
-    {
-        $this->object->setLatitude('37.5024825');
-        $this->object->setLongitude('15.0878345');
-
-        $location = $this->getMock('Geo\Location', array('getLatitude', 'getLongitude'));
-        $location->expects($this->once())
-                ->method('getLatitude')
-                ->will($this->returnValue('37.502482500000'));
-
-        $location->expects($this->once())
-                ->method('getLongitude')
-                ->will($this->returnValue('15.087834500000'));
-        // the argument is treated as a float, and presented as a floating-point number (locale aware)
-        $this->assertEquals(sprintf("%f", "0.00"), sprintf("%f", $this->object->distance($location)));
-    }
-
 }
